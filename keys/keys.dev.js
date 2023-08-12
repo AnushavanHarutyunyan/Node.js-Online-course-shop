8bf09b84b62b3f57ce510a41e5ae5a155e54aedd
Anushavan Harutyunyan <anushavan777@gmail.com>
2023-08-12 20:06:00 +0400

    keys

---
+++ keys/keys.dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MONGODB_URI:
+        'mongodb+srv://Anushavan:R1S7mz5OxyjrSYZB@cluster0.emkgk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY:
+        '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',
+    EMAIL_FROM: 'qez-hamar07@yandex.ru',
+    BASE_URL: 'http://localhost:3000',
+};
